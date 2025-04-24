9e3404c8c4cd0edafd7ff4f03d5231836f808d31
minji0000 <bluurmi@gmail.com>
2025-04-24 23:40:21 +0900

    동물앨범 만들기 2-2

--- src/components/TabBar.js
+++ src/components/TabBar.js
@@ -1,3 +1,18 @@
-export default function TabBar() {
-  
+//모듈로서 사용
+export default function TabBar({initialState, onCLick}) {
+  this.state = {};
+  this.onCLick = onCLick;
+
+  this.template = ()=> {
+    
+  }
+
+  this.render = () => {
+
+  }
+
+  this.setState = (newState)=> {
+    this.state = newState;
+    this.render();
+  }
 }
